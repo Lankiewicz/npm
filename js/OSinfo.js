50e412ccc4ff510bf6e084f5becfae5937ae5885
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-03-26 21:20:35 +0200

    update

--- js/OSinfo.js
+++ js/OSinfo.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var colors = require('colors');
 
 function getOSinfo() {
     var type = os.type();
@@ -11,14 +12,15 @@ function getOSinfo() {
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
     var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
+    console.log(colors.red('System:', type));
+    console.log(colors.yellow('Release:', release));
+    console.log(colors.blue('CPU model:', cpu));
+    console.log(colors.grey('Uptime: ~', (uptime / 60).toFixed(0), 'min'));
+    console.log(colors.green('User name:', userInfo.username));
+    console.log(colors.cyan('Home dir:', userInfo.homedir));
 }
 
 exports.print = getOSinfo;
 
 var OSinfo = require('./OSinfo');
+getOSinfo();
